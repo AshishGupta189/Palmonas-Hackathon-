cca418257ead4d896c18f009dd711d5826bfb231
Ashish Gupta <askd442@gmail.com>
2025-07-25 16:42:22 +0530

    page change wait

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -1,31 +1,56 @@
-import {Route,Routes} from "react-router-dom"
-import PageNotFound from "../pages/PageNotFound"
-import Home from "../pages/Home"
-import Profile from "../pages/Profile"
-import About from "../pages/About"
-import Login from "../components/Login"
-import Products from "../pages/Products"
-import Signup from "../components/Signup"
-import ProductDetails from "../pages/ProductDetails"
-import Wishlist from "../pages/Wishlist"
-import Cart from "../pages/Cart"
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+
+import PageNotFound from "../pages/PageNotFound";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import About from "../pages/About";
+import Login from "../components/Login";
+import Products from "../pages/Products";
+import Signup from "../components/Signup";
+import ProductDetails from "../pages/ProductDetails";
+import Wishlist from "../pages/Wishlist";
+import Cart from "../pages/Cart";
+
+// Animation variants
+const pageVariants = {
+  initial: { opacity: 0, y: 30 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.4 } },
+  exit: { opacity: 0, y: -20, transition: { duration: 0.3 } },
+};
+
+// Wrapper for animated pages
+const AnimatedPage = ({ children }) => (
+  <motion.div
+    variants={pageVariants}
+    initial="initial"
+    animate="animate"
+    exit="exit"
+    className="min-h-screen"
+  >
+    {children}
+  </motion.div>
+);
 
 const Mainroutes = () => {
+  const location = useLocation();
+
   return (
-    <Routes>
-       <Route path="/" element={<Home/>}></Route>
-       <Route path="/about" element={<About/>}></Route>
-       <Route path="/profile" element={<Profile/>}></Route>
-       <Route path="/signup" element={<Signup/>}></Route>
-       <Route path="/login" element={<Login/>}></Route>
-       <Route path="/products" element={<Products/>}></Route>
-       <Route path="/wishlist" element={<Wishlist/>}></Route>
-       <Route path="/cart" element={<Cart/>}></Route>
-       <Route path="/product/details/:id" element={<ProductDetails/>}></Route>
-       <Route path="*" element={<PageNotFound/>}></Route>
-    </Routes>
-  )
-}
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<AnimatedPage><Home /></AnimatedPage>} />
+        <Route path="/about" element={<AnimatedPage><About /></AnimatedPage>} />
+        <Route path="/profile" element={<AnimatedPage><Profile /></AnimatedPage>} />
+        <Route path="/signup" element={<AnimatedPage><Signup /></AnimatedPage>} />
+        <Route path="/login" element={<AnimatedPage><Login /></AnimatedPage>} />
+        <Route path="/products" element={<AnimatedPage><Products /></AnimatedPage>} />
+        <Route path="/wishlist" element={<AnimatedPage><Wishlist /></AnimatedPage>} />
+        <Route path="/cart" element={<AnimatedPage><Cart /></AnimatedPage>} />
+        <Route path="/product/details/:id" element={<AnimatedPage><ProductDetails /></AnimatedPage>} />
+        <Route path="*" element={<AnimatedPage><PageNotFound /></AnimatedPage>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
-export default Mainroutes
-
+export default Mainroutes;
